d836e26f3a1077d69cd50eb2e8d1e328ac19f695
Philane Msibi <Philane.msibi@dfafrica.co.za>
2020-12-01 01:04:56 +0200

    First Commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Icon, Modal } from 'semantic-ui-react';
+import { Button, Modal } from 'semantic-ui-react';
 
 const Box = (props) => {
   const {
